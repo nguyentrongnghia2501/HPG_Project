ced19494b381f1c5534120aadb6b2d630515f8b6
NghiaStaffBunbu <nghia.nguyen.trong@bunbu.vn>
2023-06-26 20:42:54 +0700

    implement base devise for User

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'static_pages#home'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
